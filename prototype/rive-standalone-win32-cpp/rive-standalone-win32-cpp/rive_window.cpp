31769f9785ad54a78badd4633fd9a8f7dc65dd38
James Clarke (WinExp Microsoft Store) <james.clarke@microsoft.com>
2025-05-27 13:30:53 -0700

    Fix build

--- prototype/rive-standalone-win32-cpp/rive-standalone-win32-cpp/rive_window.cpp
+++ prototype/rive-standalone-win32-cpp/rive-standalone-win32-cpp/rive_window.cpp
@@ -85,6 +85,9 @@ void RiveWindow::OnResize(int width, int height) {
     }
 }
 
+// Forward declaration - function is defined in dx_window.cpp
+DesktopWindowTarget CreateDesktopWindowTarget(Compositor const& compositor, HWND window);
+
 Windows::System::DispatcherQueueController RiveWindow::CreateDispatcherQueueCont()
 {
     namespace abi = ABI::Windows::System;
@@ -99,16 +102,6 @@ Windows::System::DispatcherQueueController RiveWindow::CreateDispatcherQueueCont
     Windows::System::DispatcherQueueController controller{ nullptr };
     check_hresult(CreateDispatcherQueueController(options, reinterpret_cast<abi::IDispatcherQueueController**>(put_abi(controller))));
     return controller;
-}
-
-DesktopWindowTarget CreateDesktopWindowTarget(Compositor const& compositor, HWND window)
-{
-    namespace abi = ABI::Windows::UI::Composition::Desktop;
-
-    auto interop = compositor.as<abi::ICompositorDesktopInterop>();
-    DesktopWindowTarget target{ nullptr };
-    check_hresult(interop->CreateDesktopWindowTarget(window, true, reinterpret_cast<abi::IDesktopWindowTarget**>(put_abi(target))));
-    return target;
 }
 
 void RiveWindow::PrepareVisuals()
