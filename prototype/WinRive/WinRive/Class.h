9fed9d7cdf71cfc3c46156056a5f61faaf5f3969
James Clarke (WinExp Microsoft Store) <james.clarke@microsoft.com>
2025-06-17 13:58:23 -0700

    Add WinRive stub

---
+++ prototype/WinRive/WinRive/Class.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "Class.g.h"
+
+namespace winrt::WinRive::implementation
+{
+    struct Class : ClassT<Class>
+    {
+        Class() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::WinRive::factory_implementation
+{
+    struct Class : ClassT<Class, implementation::Class>
+    {
+    };
+}
